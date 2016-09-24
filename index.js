6c589573ba29ff71316e0cc96bca8a7c20b915ba
Andrey Bukati <andrey@bukati.me>
2016-09-24 18:26:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express       = require('express'),
 
 /** SET UP **/
 const PORT          = process.env.PORT || 8080;
-global.PACKAGE_NAME = "marketplace-mailchimp-package";
+global.PACKAGE_NAME = "MailChimp;
 
 const app = express();
 app.use(bodyParser.json(({limit: '50mb'})));
